3256d9073ea7b9618ecee75ddca078bbd8a94536
najarani <nirajanpoudel77@gmail.com>
2024-01-11 22:08:04 +0545

    learnt Variable and Data types

--- firstprojectkot/src/main/kotlin/firstprogram.kt
+++ firstprojectkot/src/main/kotlin/firstprogram.kt
@@ -1,3 +1,7 @@
 fun main(){
     println("Hello World")
+    val i =1
+    val j = 2
+    val ret = i+ j
+    println(ret)
 }
---
+++ firstprojectkot/src/main/kotlin/variableAndDatatypes.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var score : Int = 12
+    var temperature : Double = 32.23
+    var isRaining : Boolean = true
+    var alphabet : Char = 'A'
+    var message : String = "Hello World"
+    println(score)
+    println(temperature)
+    println(isRaining)
+    println(alphabet)
+    println(message)
+}
